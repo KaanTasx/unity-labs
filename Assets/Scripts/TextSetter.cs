0efade7ba28d76b1fccd5ae4e4e7a4415deffbe5
KaanTasx <kaantascep@gmail.com>
2025-12-02 18:58:07 +0100

    make lab6

--- Assets/Scripts/TextSetter.cs
+++ Assets/Scripts/TextSetter.cs
@@ -1,20 +1,67 @@
 using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class TextSetter : MonoBehaviour
 {
     [SerializeField] private Text _text;
-
-    private int CountOfShows
+    private readonly string _apiUrl = "http://localhost:5237/weatherforecast";
+    
+    public void ShowWeather()
     {
-        get => PlayerPrefs.GetInt("CountOfShows", 0);
-        set => PlayerPrefs.SetInt("CountOfShows", value);
+        StartCoroutine(GetWeatherForecast());
     }
 
-    private void OnEnable()
+    private IEnumerator GetWeatherForecast()
     {
-        CountOfShows++;
-        _text.text = $"Shows: {CountOfShows}";
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(_apiUrl))
+        {
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.result != UnityWebRequest.Result.Success)
+            {
+                _text.text = "Error: " + webRequest.error;
+            }
+            else
+            {
+                string jsonResponse = webRequest.downloadHandler.text;
+                WeatherForecast[] forecasts = JsonHelper.FromJson<WeatherForecast>(jsonResponse);
+
+                _text.text = "Weather Forecast:\n\n";
+                foreach (var forecast in forecasts)
+                {
+                    _text.text += $"{forecast.date}: {forecast.temperatureC}°C ({forecast.temperatureF}°F) - {forecast.summary}\n";
+                }
+            }
+        }
+    }
+    
+    [System.Serializable]
+    public class WeatherForecast
+    {
+        public string date;
+        public int temperatureC;
+        public string summary;
+        public int temperatureF;
+    }
+
+    // Допоміжний клас для парсингу JSON у масив
+    public static class JsonHelper
+    {
+        public static T[] FromJson<T>(string json)
+        {
+            // Окремий випадок для JSON масиву
+            json = "{\"array\":" + json + "}";
+            Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
+            return wrapper.array;
+        }
+
+        [System.Serializable]
+        private class Wrapper<T>
+        {
+            public T[] array;
+        }
     }
 }
